04c36dc9bf2127f14bb1a6c03a15f335c93f1b03
Tienluc037 <nguyentienluc12a3@gmail.com>
2022-02-25 10:21:47 +0700

    thuc hanh

--- practice/routes/web.php
+++ practice/routes/web.php
@@ -12,7 +12,33 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// thuc hanh 1
 Route::get('/', function () {
-    return view('welcome');
+    echo "<h2>This is Home page</h2>";
 });
+
+Route::get('/about', function () {
+    echo "<h2>This is About page</h2>";
+});
+
+Route::get('/contact', function () {
+    echo "<h2>This is Contact page</h2>";
+});
+
+
+
+//Khi Routing thực thi thì nó có thể nhận và truyền các tham số
+Route::get('/user', function () {
+    return view('user', ['name'=>'Masud Alam']);
+});
+
+
+//truyền tham số trên thanh địa chỉ khi chạy web
+Route::get('/user/{name}', function ($name) {
+    echo "<h2>User name is $name</h2>";
+});
+
+
+//Tạo controller php artisan make:controller HomeController
+// thực thi homeController
+Route::get('/', 'App\Http\Controllers\HomeController@index');
